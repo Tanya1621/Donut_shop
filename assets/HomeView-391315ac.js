365afaa281c736587eea3b593fb878051602d6de
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2023-01-21 12:35:34 +0200

    Updates

---
+++ assets/HomeView-391315ac.js
@@ -0,0 +1,1 @@
+import{_ as o,a as t,o as a,c as s,b as n,r}from"./index-24b069c7.js";function c(_,p,i,m,d,l){const e=r("CardList");return a(),s("main",null,[n(e)])}const u=o(t,[["render",c]]);export{u as default};
